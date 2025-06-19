4fe6b7e34f9103712418430b3e7cafce80e6e23e
Sashaklim <sashaklimovic995@gmail.com>
2025-06-19 21:41:08 +0300

    4. Связаны элементы интерфейса с переменными в MainActivity.kt

--- app/src/main/java/com/example/string/MainActivity.kt
+++ app/src/main/java/com/example/string/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.string
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.*
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputString: EditText
+    private lateinit var inputN: EditText
+    private lateinit var inputChar: EditText
+    private lateinit var replaceButton: Button
+    private lateinit var resultText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputString = findViewById(R.id.inputString)
+        inputN = findViewById(R.id.inputN)
+        inputChar = findViewById(R.id.inputChar)
+        replaceButton = findViewById(R.id.replaceButton)
+        resultText = findViewById(R.id.resultText)
+
     }
 }
